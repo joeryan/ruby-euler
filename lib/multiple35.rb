5f83b9faa3dc98d30c239e8ec31e84c3fdf59b70
Joe Ryan <joe@clanryan.us>
2015-11-28 00:15:47 -0500

    create initalization for Multiple35.rb

--- lib/multiple35.rb
+++ lib/multiple35.rb
@@ -1,5 +1,13 @@
 # Multiple35, takes a range and calculates the sum of the multiples of 3 and 5
 
 class Multiple35
+  attr_accessor :total_range
 
+  def initialize (args = {})
+    if args != {}
+      self.total_range = args[:total_range]
+    else
+      self.total_range = 0
+    end
+  end
 end
--- spec/Multiple35_spec.rb
+++ spec/Multiple35_spec.rb
@@ -1,8 +1,13 @@
 require "multiple35"
 
 describe Multiple35 do
-  it "should initialize the total number of elements to use" do
+  it "should initialize the total number of elements to use to 10" do
     multis = Multiple35.new(:total_range => 10)
     multis.total_range.should == 10
+  end
+
+  it "should initialize he total number of elements to use to 100" do
+    multis = Multiple35.new(:total_range => 100)
+    multis.total_range.should == 100
   end
 end
