ea74028b99523c93227f835889b0fa2945d59d45
Joe Ryan <joe@clanryan.us>
2015-11-27 23:40:04 -0500

    create a simple passing spec, Testclass35_spec.rb

---
+++ spec/Testclass35_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+class Testclass35; end
+
+describe Testclass35 do
+  it "should be a passing class" do
+    true == true
+  end
+end
--- spec/multiple35_spec.rb
+++ spec/multiple35_spec.rb
@@ -3,10 +3,9 @@ require "spec_helper"
 class Multiple_35; end
 
 describe Mutliple_35 do
-  describe "#initialize" do
+
     it "sets the total range of numbers to use" do
       multi = Multiple_35.new(:total_range => 10)
       mutli.total_range.should == 10
-    end
   end
 end
