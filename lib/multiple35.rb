8c0b24a69ab938e96b35c9fc1f1d36d6f7bfaa61
Joe Ryan <joe@clanryan.us>
2015-11-28 00:43:20 -0500

    complete project Euler problem 1

--- lib/multiple35.rb
+++ lib/multiple35.rb
@@ -1,13 +1,24 @@
 # Multiple35, takes a range and calculates the sum of the multiples of 3 and 5
 
 class Multiple35
-  attr_accessor :total_range
+  attr_accessor :max_num
 
   def initialize (args = {})
     if args != {}
-      self.total_range = args[:total_range]
+      self.max_num = args[:max_num]
     else
       self.total_range = 0
     end
   end
+
+  def sum
+    the_sum = 0
+    for num in (0...self.max_num)
+      if num % 3 == 0 or num % 5 == 0
+        the_sum += num
+      end
+    end
+    return the_sum
+  end
+
 end
--- spec/Multiple35_spec.rb
+++ spec/Multiple35_spec.rb
@@ -1,13 +1,23 @@
 require "multiple35"
 
 describe Multiple35 do
-  it "should initialize the total number of elements to use to 10" do
-    multis = Multiple35.new(:total_range => 10)
-    multis.total_range.should == 10
+  it "should initialize the max number of elements to use to 10" do
+    multis = Multiple35.new(:max_num => 10)
+    multis.max_num.should == 10
   end
 
-  it "should initialize he total number of elements to use to 100" do
-    multis = Multiple35.new(:total_range => 100)
-    multis.total_range.should == 100
+  it "should initialize the max number of elements to use to 100" do
+    multis = Multiple35.new(:max_num => 100)
+    multis.max_num.should == 100
+  end
+
+  it "should calculate the sum of the multiples of 3 and 5 below max of 10" do
+    multis = Multiple35.new(:max_num => 10)
+    multis.sum.should == 23
+  end
+
+  it "should calculate the sum of the multiples of 3 and 5 below max of 1000" do
+    multis = Multiple35.new(:max_num => 1000)
+    multis.sum.should == 233168
   end
 end
