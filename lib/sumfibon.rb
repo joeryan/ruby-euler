97f98328b4ad80bc574ac1ff288ed13674fff492
Joe Ryan <joe@clanryan.us>
2015-11-30 13:19:09 -0500

    correct SumFibon_spec typo, add SumFibon constructor and initial sum feature

--- lib/sumfibon.rb
+++ lib/sumfibon.rb
@@ -1,3 +1,17 @@
 class SumFibon
 
+  attr_accessor :terms
+
+  def initialize (args = {})
+    if args != {}
+      self.terms = args[:max_num]
+    else
+      self.terms = 0
+    end
+  end
+
+  def evenSum
+    return 0
+  end
+
 end
