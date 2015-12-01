c52fbe18a56961139afaa0920f16918a509d9954
Joe Ryan <joe@clanryan.us>
2015-11-30 20:18:41 -0500

    update should to new rspec expect format

--- spec/Multiple35_spec.rb
+++ spec/Multiple35_spec.rb
@@ -3,21 +3,21 @@ require "multiple35"
 describe Multiple35 do
   it "should initialize the max number of elements to use to 10" do
     multis = Multiple35.new(:max_num => 10)
-    multis.max_num.should == 10
+    expect(multis.max_num).to eq(10)
   end
 
   it "should initialize the max number of elements to use to 100" do
     multis = Multiple35.new(:max_num => 100)
-    multis.max_num.should == 100
+    expect(multis.max_num).to eq(100)
   end
 
   it "should calculate the sum of the multiples of 3 and 5 below max of 10" do
     multis = Multiple35.new(:max_num => 10)
-    multis.sum.should == 23
+    expect(multis.sum).to eq(23)
   end
 
   it "should calculate the sum of the multiples of 3 and 5 below max of 1000" do
     multis = Multiple35.new(:max_num => 1000)
-    multis.sum.should == 233168
+    expect(multis.sum).to eq(233168)
   end
 end
--- spec/SumFibon_spec.rb
+++ spec/SumFibon_spec.rb
@@ -3,17 +3,17 @@ require "sumfibon"
 describe SumFibon do
   it "should initialize the number of fibonacci terms" do
     summer = SumFibon.new(:terms => 10)
-    summer.terms.should == 10
+    expect(summer.terms).to eq(10)
     print summer.terms
   end
 
   it "should sum the first (10) even fibonacci numbers" do
     summer = SumFibon.new(:terms => 10)
-    summer.evenSum.should == 44
+    expect(summer.evenSum).to eq(44)
   end
 
   it "should sum the even fibonacci numbers up to a max value of 4,000,000" do
   summer = SumFibon.new(:terms => 4000000, :maxval => 4000000)
-  summer.evenSum.should == 4613732
+  expect(summer.evenSum).to eq(4613732)
   end
 end
