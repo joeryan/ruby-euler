dd949e0520ab22bc96f7fb28b1ee3d7e59299949
Joe Ryan <joe@clanryan.us>
2015-12-05 21:47:30 -0500

    add test for final problem solution

--- spec/largestprime_spec.rb
+++ spec/largestprime_spec.rb
@@ -5,4 +5,9 @@ describe LargestPrime do
     lgPrime = LargestPrime.new({:num => 13195})
     expect(lgPrime.calc).to eq(29)
   end
+
+  it "should return the Largest Prime factor of 600851475143" do
+    lgPrime = LargestPrime.new({:num => 600851475143})
+    expect(lgPrime.calc).to eq(6857)
+  end
 end
